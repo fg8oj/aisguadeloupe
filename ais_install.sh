11cdea10cfac2496ad40b37ea64cda429c2196bc
Bertrand Demarcq FG8OJ/AG8OJ <41483500+fg8oj@users.noreply.github.com>
2019-08-28 17:15:54 -0400

    Update ais_install.sh

--- ais_install.sh
+++ ais_install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/fg8oj/aisguadeloupe/master/ais.service -O /etc/systemd/system/ais.service
+sudo wget https://github.com/fg8oj/aisguadeloupe/raw/master/aisdeco2 -O /usr/local/bin/aisdeco2
+sudo chmod +x /usr/local/bin/aisdeco2 
+wget https://raw.githubusercontent.com/fg8oj/aisguadeloupe/master/aisdeco2.service -O /etc/systemd/system/aisdeco2.service
 sudo systemctl daemon-reload
-sudo systemctl enable ais
-sudo systemctl start ais
+sudo systemctl enable aisdeco2
+sudo systemctl start aisdeco2
+
