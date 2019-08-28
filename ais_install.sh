a1608d84ffb5f7cf2c4be9a389eb65532960db09
Bertrand Demarcq FG8OJ/AG8OJ <41483500+fg8oj@users.noreply.github.com>
2019-08-28 17:25:25 -0400

    Update ais_install.sh

--- ais_install.sh
+++ ais_install.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+sudo apt update
+sudo apt upgrade
+sudo apt clean
+sudo apt install unzip php-cli php-curl git nmap socat iptraf autossh librtlsdr-dev
 sudo wget https://github.com/fg8oj/aisguadeloupe/raw/master/aisdeco2 -O /usr/local/bin/aisdeco2
 sudo chmod +x /usr/local/bin/aisdeco2 
 sudo wget https://raw.githubusercontent.com/fg8oj/aisguadeloupe/master/aisdeco2.service -O /etc/systemd/system/aisdeco2.service
 sudo systemctl daemon-reload
 sudo systemctl enable aisdeco2
 sudo systemctl start aisdeco2
-apt install unzip php-cli php-curl git nmap socat iptraf autossh librtlsdr-dev
 wget https://github.com/fg8oj/aisguadeloupe/archive/master.zip
 unzip master.zip
 
