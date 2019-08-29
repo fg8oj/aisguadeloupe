a8c721fec69b779e0844cfc3d2459451a6f4f116
Bertrand Demarcq FG8OJ/AG8OJ <41483500+fg8oj@users.noreply.github.com>
2019-08-28 20:59:56 -0400

    Update ais_install.sh

--- ais_install.sh
+++ ais_install.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
-cat <<EOF >no-rtl.conf
-blacklist dvb_usb_rtl28xxu
-blacklist rtl2832
-blacklist rtl2830
-EOF
 sudo tee /etc/modprobe.d/rtl-sdr-blacklist.conf >/dev/null << _EOF 
 blacklist dvb_usb_rtl28xxu
 blacklist e4000
 blacklist rtl2832
+blacklist rtl2830
 _EOF
 sudo mv no-rtl.conf /etc/modprobe.d/
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y 
 sudo apt clean
 sudo apt install -y unzip php-cli php-curl git nmap socat iptraf autossh librtlsdr-dev tcpdump telnet cmake build-essential
 git clone git://git.osmocom.org/rtl-sdr.git
@@ -24,12 +20,15 @@ sudo make install
 sudo ldconfig
 cd ~
 sudo cp ./rtl-sdr/rtl-sdr.rules /etc/udev/rules.d/
-sudo wget https://github.com/fg8oj/aisguadeloupe/raw/master/aisdeco2 -O /usr/local/bin/aisdeco2
-sudo chmod +x /usr/local/bin/aisdeco2 
-sudo wget https://raw.githubusercontent.com/fg8oj/aisguadeloupe/master/aisdeco2.service -O /etc/systemd/system/aisdeco2.service
+git clone https://github.com/dgiardini/rtl-ais 
+cd rtl-ais
+make
+cd ~
+sudo cp rtl-ais/rtl_ais /usr/local/bin/rtl_ais
+sudo wget https://raw.githubusercontent.com/fg8oj/aisguadeloupe/master/ais.service -O /etc/systemd/system/ais.service
 sudo systemctl daemon-reload
-sudo systemctl enable aisdeco2
-sudo systemctl start aisdeco2
+sudo systemctl enable ais
+sudo systemctl start ais
 wget https://github.com/fg8oj/aisguadeloupe/archive/master.zip
 unzip master.zip
 
